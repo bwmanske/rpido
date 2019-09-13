eaa32b99fbf48b39426276e55711d6b2912666a1
Brad Manske <brad@manske.org>
2019-09-13 14:50:15 -0500

    changed file to have 4 spaces per tab

--- rpido-target.sh
+++ rpido-target.sh
@@ -2,228 +2,229 @@
 # 
 
 get_vnc() {
-  if systemctl status vncserver-x11-serviced.service  | grep -q inactive; then
-    echo 1
-  else
-    echo 0
-  fi
+    if systemctl status vncserver-x11-serviced.service  | grep -q inactive; then
+        echo 1
+    else
+        echo 0
+    fi
 }
 
 do_vnc() {
-  DEFAULT=--defaultno
-  if [ $(get_vnc) -eq 0 ]; then
-    DEFAULT=
-  fi
-  if [ "$INTERACTIVE" = True ]; then
-    whiptail --yesno "Would you like the VNC Server to be enabled?" $DEFAULT 20 60 2
-    RET=$?
-  else
-    RET=$1
-  fi
-  if [ $RET -eq 0 ]; then
-    if [ ! -d /usr/share/doc/realvnc-vnc-server ] ; then
-        apt-get install realvnc-vnc-server
+    DEFAULT=--defaultno
+    if [ $(get_vnc) -eq 0 ]; then
+        DEFAULT=
     fi
-    systemctl enable vncserver-x11-serviced.service &&
-    systemctl start vncserver-x11-serviced.service &&
-    STATUS=enabled
-  elif [ $RET -eq 1 ]; then
-    systemctl disable vncserver-x11-serviced.service &&
-    systemctl stop vncserver-x11-serviced.service &&
-    STATUS=disabled
-  else
-    return $RET
-  fi
-  if [ "$INTERACTIVE" = True ]; then
-    whiptail --msgbox "The VNC Server is $STATUS" 20 60 1
-  fi
+    if [ "$INTERACTIVE" = True ]; then
+        whiptail --yesno "Would you like the VNC Server to be enabled?" $DEFAULT 20 60 2
+        RET=$?
+    else
+        RET=$1
+    fi
+    if [ $RET -eq 0 ]; then
+        if [ ! -d /usr/share/doc/realvnc-vnc-server ] ; then
+            apt-get install realvnc-vnc-server
+        fi
+        systemctl enable vncserver-x11-serviced.service &&
+        systemctl start vncserver-x11-serviced.service &&
+        STATUS=enabled
+    elif [ $RET -eq 1 ]; then
+        systemctl disable vncserver-x11-serviced.service &&
+        systemctl stop vncserver-x11-serviced.service &&
+        STATUS=disabled
+    else
+        return $RET
+    fi
+    if [ "$INTERACTIVE" = True ]; then
+        whiptail --msgbox "The VNC Server is $STATUS" 20 60 1
+    fi
 }
 
 get_spi() {
-  if grep -q -E "^(device_tree_param|dtparam)=([^,]*,)*spi(=(on|true|yes|1))?(,.*)?$" $CONFIG; then
-    echo 0
-  else
-    echo 1
-  fi
+    if grep -q -E "^(device_tree_param|dtparam)=([^,]*,)*spi(=(on|true|yes|1))?(,.*)?$" $CONFIG; then
+        echo 0
+    else
+        echo 1
+    fi
 }
 
 do_spi() {
-  DEFAULT=--defaultno
-  if [ $(get_spi) -eq 0 ]; then
-    DEFAULT=
-  fi
-  if [ "$INTERACTIVE" = True ]; then
-    whiptail --yesno "Would you like the SPI interface to be enabled?" $DEFAULT 20 60 2
-    RET=$?
-  else
-    RET=$1
-  fi
-  if [ $RET -eq 0 ]; then
-    SETTING=on
-    STATUS=enabled
-  elif [ $RET -eq 1 ]; then
-    SETTING=off
-    STATUS=disabled
-  else
-    return $RET
-  fi
+    DEFAULT=--defaultno
+    if [ $(get_spi) -eq 0 ]; then
+        DEFAULT=
+    fi
+    if [ "$INTERACTIVE" = True ]; then
+        whiptail --yesno "Would you like the SPI interface to be enabled?" $DEFAULT 20 60 2
+        RET=$?
+    else
+        RET=$1
+    fi
+    if [ $RET -eq 0 ]; then
+        SETTING=on
+        STATUS=enabled
+    elif [ $RET -eq 1 ]; then
+        SETTING=off
+        STATUS=disabled
+    else
+        return $RET
+    fi
 
-  set_config_var dtparam=spi $SETTING $CONFIG &&
-  if ! [ -e $BLACKLIST ]; then
-    touch $BLACKLIST
-  fi
-  sed $BLACKLIST -i -e "s/^\(blacklist[[:space:]]*spi[-_]bcm2708\)/#\1/"
-  dtparam spi=$SETTING
+    set_config_var dtparam=spi $SETTING $CONFIG &&
+    if ! [ -e $BLACKLIST ]; then
+        touch $BLACKLIST
+    fi
+    sed $BLACKLIST -i -e "s/^\(blacklist[[:space:]]*spi[-_]bcm2708\)/#\1/"
+    dtparam spi=$SETTING
 
-  if [ "$INTERACTIVE" = True ]; then
-    whiptail --msgbox "The SPI interface is $STATUS" 20 60 1
-  fi
+    if [ "$INTERACTIVE" = True ]; then
+        whiptail --msgbox "The SPI interface is $STATUS" 20 60 1
+    fi
 }
 
 get_i2c() {
-  if grep -q -E "^(device_tree_param|dtparam)=([^,]*,)*i2c(_arm)?(=(on|true|yes|1))?(,.*)?$" $CONFIG; then
-    echo 0
-  else
-    echo 1
-  fi
+    if grep -q -E "^(device_tree_param|dtparam)=([^,]*,)*i2c(_arm)?(=(on|true|yes|1))?(,.*)?$" $CONFIG; then
+        echo 0
+    else
+        echo 1
+    fi
 }
 
 do_i2c() {
-  DEFAULT=--defaultno
-  if [ $(get_i2c) -eq 0 ]; then
-    DEFAULT=
-  fi
-  if [ "$INTERACTIVE" = True ]; then
-    whiptail --yesno "Would you like the ARM I2C interface to be enabled?" $DEFAULT 20 60 2
-    RET=$?
-  else
-    RET=$1
-  fi
-  if [ $RET -eq 0 ]; then
-    SETTING=on
-    STATUS=enabled
-  elif [ $RET -eq 1 ]; then
-    SETTING=off
-    STATUS=disabled
-  else
-    return $RET
-  fi
-
-  set_config_var dtparam=i2c_arm $SETTING $CONFIG &&
-  if ! [ -e $BLACKLIST ]; then
-    touch $BLACKLIST
-  fi
-  sed $BLACKLIST -i -e "s/^\(blacklist[[:space:]]*i2c[-_]bcm2708\)/#\1/"
-  sed /etc/modules -i -e "s/^#[[:space:]]*\(i2c[-_]dev\)/\1/"
-  if ! grep -q "^i2c[-_]dev" /etc/modules; then
-    printf "i2c-dev\n" >> /etc/modules
-  fi
-  dtparam i2c_arm=$SETTING
-  modprobe i2c-dev
-
-  if [ "$INTERACTIVE" = True ]; then
-    whiptail --msgbox "The ARM I2C interface is $STATUS" 20 60 1
-  fi
+    DEFAULT=--defaultno
+    if [ $(get_i2c) -eq 0 ]; then
+        DEFAULT=
+    fi
+    if [ "$INTERACTIVE" = True ]; then
+        whiptail --yesno "Would you like the ARM I2C interface to be enabled?" $DEFAULT 20 60 2
+        RET=$?
+    else
+        RET=$1
+    fi
+    if [ $RET -eq 0 ]; then
+        SETTING=on
+        STATUS=enabled
+    elif [ $RET -eq 1 ]; then
+        SETTING=off
+        STATUS=disabled
+    else
+        return $RET
+    fi
+  
+    set_config_var dtparam=i2c_arm $SETTING $CONFIG &&
+    if ! [ -e $BLACKLIST ]; then
+        touch $BLACKLIST
+    fi
+    sed $BLACKLIST -i -e "s/^\(blacklist[[:space:]]*i2c[-_]bcm2708\)/#\1/"
+    sed /etc/modules -i -e "s/^#[[:space:]]*\(i2c[-_]dev\)/\1/"
+    if ! grep -q "^i2c[-_]dev" /etc/modules; then
+        printf "i2c-dev\n" >> /etc/modules
+    fi
+    dtparam i2c_arm=$SETTING
+    modprobe i2c-dev
+  
+    if [ "$INTERACTIVE" = True ]; then
+        whiptail --msgbox "The ARM I2C interface is $STATUS" 20 60 1
+    fi
 }
 
 get_serial() {
-  if grep -q -E "console=(serial0|ttyAMA0|ttyS0)" $CMDLINE ; then
-    echo 0
-  else
-    echo 1
-  fi
+    if grep -q -E "console=(serial0|ttyAMA0|ttyS0)" $CMDLINE ; then
+        echo 0
+    else
+        echo 1
+    fi
 }
 
 get_serial_hw() {
-  if grep -q -E "^enable_uart=1" $CONFIG ; then
-    echo 0
-  elif grep -q -E "^enable_uart=0" $CONFIG ; then
-    echo 1
-  elif [ -e /dev/serial0 ] ; then
-    echo 0
-  else
-    echo 1
-  fi
+    if grep -q -E "^enable_uart=1" $CONFIG ; then
+        echo 0
+    elif grep -q -E "^enable_uart=0" $CONFIG ; then
+        echo 1
+    elif [ -e /dev/serial0 ] ; then
+        echo 0
+    else
+        echo 1
+    fi
 }
 
 do_serial() {
-  DEFAULTS=--defaultno
-  DEFAULTH=--defaultno
-  CURRENTS=0
-  CURRENTH=0
-  if [ $(get_serial) -eq 0 ]; then
-      DEFAULTS=
-      CURRENTS=1
-  fi
-  if [ $(get_serial_hw) -eq 0 ]; then
-      DEFAULTH=
-      CURRENTH=1
-  fi
-  if [ "$INTERACTIVE" = True ]; then
-    whiptail --yesno "Would you like a login shell to be accessible over serial?" $DEFAULTS 20 60 2
-    RET=$?
-  else
-    RET=$1
-  fi
-  if [ $RET -eq $CURRENTS ]; then
-    ASK_TO_REBOOT=1
-  fi
-  if [ $RET -eq 0 ]; then
-    if grep -q "console=ttyAMA0" $CMDLINE ; then
-      if [ -e /proc/device-tree/aliases/serial0 ]; then
-        sed -i $CMDLINE -e "s/console=ttyAMA0/console=serial0/"
-      fi
-    elif ! grep -q "console=ttyAMA0" $CMDLINE && ! grep -q "console=serial0" $CMDLINE ; then
-      if [ -e /proc/device-tree/aliases/serial0 ]; then
-        sed -i $CMDLINE -e "s/root=/console=serial0,115200 root=/"
-      else
-        sed -i $CMDLINE -e "s/root=/console=ttyAMA0,115200 root=/"
-      fi
+    DEFAULTS=--defaultno
+    DEFAULTH=--defaultno
+    CURRENTS=0
+    CURRENTH=0
+    if [ $(get_serial) -eq 0 ]; then
+        DEFAULTS=
+        CURRENTS=1
     fi
-    set_config_var enable_uart 1 $CONFIG
-    SSTATUS=enabled
-    HSTATUS=enabled
-  elif [ $RET -eq 1 ] || [ $RET -eq 2 ]; then
-    sed -i $CMDLINE -e "s/console=ttyAMA0,[0-9]\+ //"
-    sed -i $CMDLINE -e "s/console=serial0,[0-9]\+ //"
-    SSTATUS=disabled
+    if [ $(get_serial_hw) -eq 0 ]; then
+        DEFAULTH=
+        CURRENTH=1
+    fi
     if [ "$INTERACTIVE" = True ]; then
-      whiptail --yesno "Would you like the serial port hardware to be enabled?" $DEFAULTH 20 60 2
-      RET=$?
+        whiptail --yesno "Would you like a login shell to be accessible over serial?" $DEFAULTS 20 60 2
+        RET=$?
     else
-      RET=$((2-$RET))
+        RET=$1
     fi
-    if [ $RET -eq $CURRENTH ]; then
-     ASK_TO_REBOOT=1
+    if [ $RET -eq $CURRENTS ]; then
+        ASK_TO_REBOOT=1
     fi
     if [ $RET -eq 0 ]; then
-      set_config_var enable_uart 1 $CONFIG
-      HSTATUS=enabled
-    elif [ $RET -eq 1 ]; then
-      set_config_var enable_uart 0 $CONFIG
-      HSTATUS=disabled
+        if grep -q "console=ttyAMA0" $CMDLINE ; then
+            if [ -e /proc/device-tree/aliases/serial0 ]; then
+                sed -i $CMDLINE -e "s/console=ttyAMA0/console=serial0/"
+            fi
+        elif ! grep -q "console=ttyAMA0" $CMDLINE && ! grep -q "console=serial0" $CMDLINE ; then
+            if [ -e /proc/device-tree/aliases/serial0 ]; then
+                sed -i $CMDLINE -e "s/root=/console=serial0,115200 root=/"
+            else
+                sed -i $CMDLINE -e "s/root=/console=ttyAMA0,115200 root=/"
+            fi
+        fi
+        set_config_var enable_uart 1 $CONFIG
+        SSTATUS=enabled
+        HSTATUS=enabled
+    elif [ $RET -eq 1 ] || [ $RET -eq 2 ]; then
+        sed -i $CMDLINE -e "s/console=ttyAMA0,[0-9]\+ //"
+        sed -i $CMDLINE -e "s/console=serial0,[0-9]\+ //"
+        SSTATUS=disabled
+        if [ "$INTERACTIVE" = True ]; then
+            whiptail --yesno "Would you like the serial port hardware to be enabled?" $DEFAULTH 20 60 2
+            RET=$?
+        else
+            RET=$((2-$RET))
+        fi
+        if [ $RET -eq $CURRENTH ]; then
+            ASK_TO_REBOOT=1
+        fi
+        if [ $RET -eq 0 ]; then
+            set_config_var enable_uart 1 $CONFIG
+            HSTATUS=enabled
+        elif [ $RET -eq 1 ]; then
+            set_config_var enable_uart 0 $CONFIG
+            HSTATUS=disabled
+        else
+            return $RET
+        fi
     else
-      return $RET
+        return $RET
     fi
-  else
-    return $RET
-  fi
-  if [ "$INTERACTIVE" = True ]; then
-      whiptail --msgbox "The serial login shell is $SSTATUS\nThe serial interface is $HSTATUS" 20 60 1
-  fi
+    if [ "$INTERACTIVE" = True ]; then
+        whiptail --msgbox "The serial login shell is $SSTATUS\nThe serial interface is $HSTATUS" 20 60 1
+    fi
 }
 
 disable_raspi_config_at_boot() {
-  if [ -e /etc/profile.d/raspi-config.sh ]; then
-    rm -f /etc/profile.d/raspi-config.sh
-    if [ -e /etc/systemd/system/getty@tty1.service.d/raspi-config-override.conf ]; then
-      rm /etc/systemd/system/getty@tty1.service.d/raspi-config-override.conf
+    if [ -e /etc/profile.d/raspi-config.sh ]; then
+        rm -f /etc/profile.d/raspi-config.sh
+        if [ -e /etc/systemd/system/getty@tty1.service.d/raspi-config-override.conf ]; then
+            rm /etc/systemd/system/getty@tty1.service.d/raspi-config-override.conf
+        fi
+        telinit q
     fi
-    telinit q
-  fi
 }
 
 Set_Target_VNC_enable () {
+    get_vnc
     echo -n "VNC=$Target_VNC_enable "
     case $Target_VNC_enable in
         D) echo "Disable" ;;
@@ -234,6 +235,7 @@ Set_Target_VNC_enable () {
 }
 
 Set_Target_SPI_enable () {
+    get_spi
     echo -n "SPI=$Target_SPI_enable "
     case $Target_SPI_enable in
         D) echo "Disable" ;;
@@ -244,6 +246,7 @@ Set_Target_SPI_enable () {
 }
 
 Set_Target_I2C_enable () {
+    get_i2c
     echo -n "I2C=$Target_I2C_enable "
     case $Target_I2C_enable in
         D) echo "Disable" ;;
@@ -254,6 +257,7 @@ Set_Target_I2C_enable () {
 }
 
 Set_Target_Serial_enable () {
+    get_serial
     echo -n "Serial=$Target_Serial_enable "
     case $Target_Serial_enable in
         D) echo "Disable" ;;
